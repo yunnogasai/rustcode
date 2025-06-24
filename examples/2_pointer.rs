3289665bb78e4f442da21f7a2e2e46ff4939b15d
yunogasai <yunogasai728728@gmail.com>
2025-06-24 13:17:16 +0900

    add

--- examples/2_pointer.rs
+++ examples/2_pointer.rs
@@ -1,5 +1,4 @@
 use std::mem::{size_of, size_of_val};
-
 trait Trait {}
 
 struct Struct;
@@ -8,17 +7,20 @@ impl Trait for Struct {}
 fn main() {
     let yuno = 728_u32;
     let gasai = &yuno;
-    println!("{gasai:p}"); //0x1234567890
+    println!("gasai:{gasai:p}"); //0x1234567890
 
-    let str: &str = "rust";
-    println!("{str:p}"); //Pointer { addr: 0x123456789012, metadata: 4 }
+    let strs: &str = "rust";
+    println!("strs:{strs:p}"); //Pointer { addr: 0x123456789012, metadata: 4 }
+
+    let boxs: Box<str> = strs.into();
+    println!("boxs:{boxs:p}"); //Pointer { addr: 0x123456789012, metadata: 4 }
 
     let struct_ = Struct;
     let trait_obj: &dyn Trait = &struct_; //Pointer { addr: 0x1234567890, metadata: DynMetadata(0x123456789012) }
-    println!("{trait_obj:p}");
+    println!("trait_obj:{trait_obj:p}");
 
     assert_eq!(size_of_val(trait_obj), 0);
-    assert_eq!(size_of_val(str), str.len());
+    assert_eq!(size_of_val(strs), strs.len());
     assert_eq!(size_of::<&str>(), 16);
     assert_eq!(size_of::<&dyn Trait>(), 16);
     assert_eq!(size_of::<&str>(), size_of::<&dyn Trait>());
