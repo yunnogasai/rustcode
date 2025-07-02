c5155bc545ed4c7e35ea2719e2aceaf1f36d5a66
yunogasai <yunogasai728728@gmail.com>
2025-07-02 22:43:21 +0900

    5

--- examples/5_pattern_matching.rs
+++ examples/5_pattern_matching.rs
@@ -1,1 +1,28 @@
-fn main() {}
+enum Test {
+    A,
+    B(u32),
+    C(f32),
+    D(),
+}
+fn main() {
+    let a = Test::D();
+
+    let b = match a {
+        Test::A => 1,
+        Test::B(_) => 2,
+        Test::C(_) => 3,
+        Test::D() => 4,
+    };
+    assert_eq!(b, 4);
+
+    let c = Some(1);
+    if let Some(num) = c {
+        assert_eq!(num, 1);
+    }
+
+    let d = Test::B(8u32);
+    let Test::B(e) = d else {
+        return;
+    };
+    assert_eq!(e, 8);
+}
